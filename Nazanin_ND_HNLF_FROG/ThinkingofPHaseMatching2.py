180de19e30c49b72cbee5ff55aab58c6529b2805
Peter <pech6593@colorado.edu>
2021-01-31 00:01:07 -0700

    update

--- Nazanin_ND_HNLF_FROG/ThinkingofPHaseMatching2.py
+++ Nazanin_ND_HNLF_FROG/ThinkingofPHaseMatching2.py
@@ -5,14 +5,14 @@ import clipboard_and_style_sheet
 
 clipboard_and_style_sheet.style_sheet()
 
-c = 299792458
-
 
 def refractive_index(wl_um):
     return DengSellmeier(24.5).n(wl_um * 1e3)
 
 
 class DFGPHaseMismatch:
+    c = 299792458
+
     def __init__(self, wl1_um, wl2_um):
         """
         wl1_um: shorter wavelength
@@ -32,19 +32,19 @@ class DFGPHaseMismatch:
 
         self.dk = self.k1 - self.k2 - self.k3
 
-        self.v1 = c / self.n1
-        self.v2 = c / self.n2
-        self.v3 = c / self.n3
+        self.v1 = self.c / self.n1
+        self.v2 = self.c / self.n2
+        self.v3 = self.c / self.n3
 
-    def plot1d_dfg_wl(self, ax=None):
+    def plot1D_dfg_wl(self, ax=None):
         if ax is None:
             fig, ax = plt.subplots(1, 1)
 
         ax.plot(self.wl1_um, self.wl3_um)
         ax.set_xlabel("wavelength ($\mathrm{\mu m}$)")
-        ax.set_ylabel("DFG wavelength ($\mathrm{\mu m}$")
+        ax.set_ylabel("DFG wavelength ($\mathrm{\mu m}$)")
 
-    def plot1d_dk(self, ax=None):
+    def plot1D_dk(self, ax=None):
         if ax is None:
             fig, ax = plt.subplots(1, 1)
 
@@ -52,18 +52,31 @@ class DFGPHaseMismatch:
         ax.set_ylabel("phase mismatch (1/$\mathrm{\mu m}$)")
         ax.set_xlabel("wavelength ($\mathrm{\mu m}$)")
 
-    def plot1d_walkoff(self, ax=None):
+    def plot1D_walkoff(self, ax=None):
         if ax is None:
             fig, ax = plt.subplots(1, 1)
-        ax.plot(self.wl1_um, (1e-3 * 1e9 / abs(self.v1 - self.v2)))
+        ax.plot(self.wl1_um, 1e-3 * (1 / self.v1 - 1 / self.v2) * 1e15)
         ax.set_xlabel("wavelength ($\mathrm{\mu m}$)")
-        ax.set_ylabel("walk-off after 1mm (ns)")
+        ax.set_ylabel("walk-off after 1mm (fs)")
+
+    def get_limit_wls1D(self, wl1_um, wl2_um):
+        ind = np.where(np.logical_and(self.wl3_um >= wl1_um, self.wl3_um <= wl2_um))
+        limited_class = DFGPHaseMismatch(self.wl1_um[ind], self.wl2_um)
+        return limited_class, self.wl1_um[ind][[0, -1]]
 
 
 wl2 = 1.56
 wl1 = np.linspace(0.75, 1.25, 1000)
 run = DFGPHaseMismatch(wl1, wl2)
 
-run.plot1d_dfg_wl()
-run.plot1d_walkoff()
-run.plot1d_dk()
+# fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(17, 5))
+# run.plot1D_dfg_wl(ax1)
+# run.plot1D_walkoff(ax2)
+# run.plot1D_dk(ax3)
+
+limit, (ll, ul) = run.get_limit_wls1D(3, 5)
+
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(17, 5))
+limit.plot1D_dfg_wl(ax1)
+limit.plot1D_walkoff(ax2)
+limit.plot1D_dk(ax3)
